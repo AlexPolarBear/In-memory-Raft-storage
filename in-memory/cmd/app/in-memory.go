e3ab148423aded4256e7c3be1e3848bf1456e7f5
AlexPolarBear <durn_alex@mail.ru>
2024-05-29 19:37:54 +0300

    First work implementation of Raft

--- in-memory/cmd/app/in-memory.go
+++ in-memory/cmd/app/in-memory.go
@@ -32,12 +32,10 @@ type raftNode struct {
 	raft  *raft.Raft
 }
 
-// Restore implements raft.FSM.
 func (r *raftNode) Restore(snapshot io.ReadCloser) error {
 	panic("unimplemented")
 }
 
-// Snapshot implements raft.FSM.
 func (r *raftNode) Snapshot() (raft.FSMSnapshot, error) {
 	panic("unimplemented")
 }
@@ -73,7 +71,6 @@ func (r *raftNode) Apply(log *raft.Log) interface{} {
 	return nil
 }
 
-// Реализация интерфейсов raft.LogStore и raft.StableStore
 type RaftLogStore struct {
 	logs map[uint64]*raft.Log
 }
@@ -202,11 +199,10 @@ func loadDataFromFile(s *services.InMemoryStore, filename string) error {
 
 func main() {
 	store := services.NewInMemoryStore()
-	// Create an hclog.Logger instance
 	logger := hclog.New(&hclog.LoggerOptions{
 		Name:   "raft",
 		Output: os.Stderr,
-		Level:  hclog.LevelFromString("DEBUG"), // Adjust log level as needed
+		Level:  hclog.LevelFromString("DEBUG"),
 	})
 
 	raftConfig := raft.DefaultConfig()
@@ -225,53 +221,45 @@ func main() {
 	}
 	defer stableStore.Close()
 
-	// Создание хранилища памяти для снимков
 	snapshotStore := raft.NewDiscardSnapshotStore()
 
-	// Создание конечного автомата для обработки операций Raft
 	fsm := &raftNode{store: store}
 
-	// Создание конфигурации транспорта Raft
 	transport, err := raft.NewTCPTransport("localhost:7000", nil, 3, 10*time.Second, os.Stderr)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// Создание экземпляра Raft и его запуск
 	raftNode, err := raft.NewRaft(raftConfig, fsm, logStore, stableStore, snapshotStore, transport)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// Обработка входящих сообщений Raft
 	go func() {
 		for {
 			appliedIndex := raftNode.AppliedIndex()
 			for i := raftNode.LastIndex(); i <= appliedIndex; i++ {
-				// Получение примененной команды
+
 				logEntry, err := NewRaftLogStore().GetLogEntries(i, 10)
 				if err != nil {
 					log.Printf("Failed to get applied log at index %d: %s", i, err)
 					continue
 				}
 				if logEntry == nil || len(logEntry) == 0 {
-					// Ignore empty log entries
 					continue
 				}
-				// Ваша логика обработки примененной команды здесь
+
 				command := logEntry[len(logEntry)-1]
 				// if !ok {
 				// 	log.Printf("Failed to assert command type")
 				// 	continue
 				// }
-				// Обработка примененной команды
 				log.Printf("Applied command: %+v", command)
 			}
-			time.Sleep(100 * time.Millisecond) // Добавьте небольшую паузу перед следующей итерацией
+			time.Sleep(100 * time.Millisecond)
 		}
 	}()
 
-	// Присоединение к кластеру Raft
 	configuration := raft.Configuration{
 		Servers: []raft.Server{
 			{
